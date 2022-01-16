52e2a41921575e426905d02bd57fe34b89b2734d
Mike <turetskiy5002@gmail.com>
2022-01-16 01:22:37 +0100

    first commit

---
+++ savings.py
@@ -0,0 +1,12 @@
+from email import header
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Savings counter")
+root.geometry("500x500")
+test = tk.Label(root, text="Red", bg="red", fg="white")
+test.pack(side=tk.RIGHT)
+
+
+
+root.mainloop()
