062f01538a81f1d8005f4ac670fb0852300d263e
Mike <turetskiy5002@gmail.com>
2022-01-23 17:56:05 +0100

    added tabs

--- savings.py
+++ savings.py
@@ -1,12 +1,25 @@
-from email import header
-import tkinter as tk
-
+import tkinter as tk                    
+from tkinter import ttk
+  
+  
 root = tk.Tk()
-root.title("Savings counter")
-root.geometry("500x500")
-test = tk.Label(root, text="Red", bg="red", fg="white")
-test.pack(side=tk.RIGHT)
+root.title("Tab Widget")
+tabControl = ttk.Notebook(root)
+  
+tab1 = ttk.Frame(tabControl)
+tab2 = ttk.Frame(tabControl)
+  
+tabControl.add(tab1, text ='Tab 1')
+tabControl.add(tab2, text ='Tab 2')
+tabControl.pack(expand = 1, fill ="both")
+  
+ttk.Label(tab1, text ="Welcome to GeeksForGeeks").grid(column = 0, row = 0,padx = 30,pady = 30)  
+def create_new_tab():
+    tab = ttk.Frame(tabControl)
+    tabControl.add(tab, text= "Workspace {}")
 
-
-
-root.mainloop()
+new_tab = tk.Button(tab1, text="New tab", command=create_new_tab)
+new_tab.grid(column = 0, row = 0,padx = 30,pady = 30) 
+ttk.Label(tab2, text ="Lets dive into the world of computers").grid(column = 0,row = 0, padx = 30,pady = 30)
+  
+root.mainloop() 
